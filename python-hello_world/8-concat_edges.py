01bbe7fd6593d3931eb0ed60e2476b9a63148627
abenigne <a.benigne@alustudent.com>
2026-06-25 00:08:34 +0200

    python tasks

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[36:67] + str[100:105] + str[0:6])
+print(str)
