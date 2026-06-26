3dafa0abec240c4d5775c1b9336dd72a6a044640
Benigne Ingabire Akayesu <a.benigne@alustudent.com>
2026-06-27 00:15:49 +0200

    fixing task 8 in VS Code

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-print(word)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[106:112] + str[0:6])
