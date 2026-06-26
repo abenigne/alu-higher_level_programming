a905254620045ea8faa3301ef58a79e8be9b268e
abenigne <a.benigne@alustudent.com>
2026-06-26 23:48:43 +0200

    fixing task 6 and 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[10:40] + str[92:] + str[58:70])
+word = "Python"
+print("object-oriented programming with {}".format(word))
