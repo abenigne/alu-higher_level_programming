051ba7f52b468a06ca9fc0e2ad1494e46d8d0bd8
abenigne <a.benigne@alustudent.com>
2026-06-27 11:55:38 +0200

    edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-word = "object-oriented programming with Python"
-print(word[:26] + word[26:])
-# noqa
-# end
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[105:111] + str[0:6])
+# done
