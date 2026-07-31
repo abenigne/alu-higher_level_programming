367d93da62f9238f297f9be22322aab791199aae
abenigne <a.benigne@alustudent.com>
2026-07-31 13:34:17 +0200

    adding the fourth file

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request with a custom header to the URL passed as argument and displays the response body
+# sends a GET request with the X-HolbertonSchool-User-Id header to the URL passed as argument and displays the response body
 curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
