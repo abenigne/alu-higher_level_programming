20441c4ac7069887c5091f73cd5d22916c392fcf
abenigne <a.benigne@alustudent.com>
2026-07-31 12:23:00 +0200

    python-network_1: fix hostname to intranet.hbtn.io in 4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module that fetches the ALU intranet status endpoint using requests."""
+"""Module that fetches the intranet status endpoint using requests."""
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alu-intranet.hbtn.io/status")
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
