826d08910d736e12742171d307ca68084d74b096
abenigne <a.benigne@alustudent.com>
2026-08-01 15:25:39 +0200

    Add explicit GET method to Task 4 header script

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL with a custom header and displays the body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to a URL with a custom header
+curl -s -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
