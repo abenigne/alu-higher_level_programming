1a00b7fa0f92287b3d9d7c9df672fba14a1cc77e
abenigne <a.benigne@alustudent.com>
2026-08-01 20:57:10 +0200

    Fix 0-privileges.sql listing grants for user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost.
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost.
+-- Script to list grants for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Script to list grants for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
