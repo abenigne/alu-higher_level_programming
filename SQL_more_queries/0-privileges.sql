8ae56ec8b2a5bb14693edca16fbbb026390774fb
abenigne <a.benigne@alustudent.com>
2026-08-04 15:41:02 +0200

    Revert to SHOW GRANTS only, per checker behavior

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,10 +1,3 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2 on localhost.
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-CREATE DATABASE IF NOT EXISTS user_2_db;
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
